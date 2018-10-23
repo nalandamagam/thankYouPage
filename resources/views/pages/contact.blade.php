59d1c6f48763acc190056da8f46a35f75e546c96
nalandamagam <nalandachowdary1993@gmail.com>
2018-10-23 19:59:10 -0400

    Added required code to contact form

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <h1>This is my contact page</h1>
+    <p class="lead">Please use this form to contact site owner.</p>
+    <form action="/contact" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="Email">Email address</label>
+            <input name="email" class="form-control" id="Email" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="Body">Type your comments here</label>
+            <textarea name="body" class="form-control" id="Body" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </form>
+
+@endsection
