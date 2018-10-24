67116e25f4fbb690f7ab20e757a3e37c1b398a3b
nalandamagam <nalandachowdary1993@gmail.com>
2018-10-23 20:28:27 -0400

    Edited web.php file

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -8,11 +8,11 @@
     <form action="/contact" method="post">
         @csrf
         <div class="form-group">
-            <label for="Email">Email address</label>
+            <label for="email">Email address</label>
             <input name="email" class="form-control" id="Email" placeholder="name@example.com">
         </div>
         <div class="form-group">
-            <label for="Body">Type your comments here</label>
+            <label for="body">Message</label>
             <textarea name="body" class="form-control" id="Body" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary mb-2">Submit</button>
